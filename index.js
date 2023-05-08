876327c6769a884fb2e58c3c416c74db71b63898
stazgmina <stazgmina@gmail.com>
2023-05-08 14:30:07 +0200

    finished

--- index.js
+++ index.js
@@ -1,38 +1,59 @@
-const PORT = 8000
-import axios from 'axios'
-import * as cheerio from 'cheerio'
-import express  from 'express'
+const PORT = 3003 || 8000
 import cors from 'cors'
-import fs from 'node:fs'
+import axios from 'axios'
+import express  from 'express'
+import iconv from 'iconv-lite'
+import * as cheerio from 'cheerio'
 
 const app = express()
 app.use(cors())
 
 const url = 'https://gmina.swidnica.pl'
-const articles = []
-let text = ``
 
-let options = {    charset: 'utf8', responseEncodig: 'utf8'}
+const fetchArticles = async () => {
+    const response = await axios.get(url, {responseType: 'arraybuffer'})
+    const html = iconv.decode(Buffer.from(response.data), 'iso-8859-2')
 
-axios(url,options).then(response => {
-    const html = response.data
-    const $ = cheerio.load(html,{ decodeEntities: true })
+    const $ = cheerio.load(html)
 
-    $('a.contentpagetitle').each(function(){
-        const title = $(this).text()
-        articles.push({title: title, url: url})
-    })
+    const articles = $('a.contentpagetitle').map((i, element) => ({
+        title: $(element).text().trim(),
+        url: $(element).attr('href')
+    })).get()
 
-    articles.forEach(element => {
-        text += ` ${element.title} - ${element.url} `
-    })
+    for(const article of articles){
+        const response2 = await axios.get(article.url, {responseType: 'arraybuffer'})
+        const html2 = iconv.decode(Buffer.from(response2.data), 'iso-8859-2')
 
-    fs.writeFile('./bruh.txt', text, err => {
-        if (err) {
-          console.error(err);
-        }
-    })
+        const $$ = cheerio.load(html2)
 
+        article.img = $$('p').find('img').attr('src')
+        article.text = $$('p').find('b').text().trim()
+    }
+
+    return articles
+}
+
+app.get('/', async (req, res) => {
+    const articles = await fetchArticles()
+
+    console.log(articles)
+
+    const html = articles.map(article => (
+        `<a href='${article.url}'>
+            <div style='border: 1px solid black; width: 350px; text-align: center; padding: .5rem;'>
+                <h1>${article.title}</h1>
+                <img style='width: 100%;' src='${article.img}' alt='${article.title}'/>
+                <p>${article.text}</p>
+            </div>
+        </a>`
+    )).join('')
+
+    res.send(
+        `<div style='display: flex; flex-direction: column; align-items: center; gap: 2rem;'>
+            ${html}    
+        </div>`
+    )
 })
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running: ${PORT}`))
